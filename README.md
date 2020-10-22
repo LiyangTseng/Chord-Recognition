26164572df4ed98701a9309a3e7e76fac3bed715
LiyangTseng <k00441tsengliyung@gmail.com>
2020-10-22 10:57:08 +0800

    evaluation.py finished

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Chord-Recognition
+
+## File usage
+<code>download_audios.py: </code> download wav files from Youtube  
+<code>evaluation.py </code> using trained model to show tesing accuracy. The results would be stored at <code> score.csv </code> with the following format
+> song_number, estimate accuracy, song_name
+
+
--- test.py
+++ test.py
@@ -49,82 +49,91 @@ audio_paths = get_audio_paths(args.audio_dir)
 for i, audio_path in enumerate(audio_paths):
     logger.info("======== %d of %d in progress ========" % (i + 1, len(audio_paths)))
     # Load mp3
-    feature, feature_per_second, song_length_second = audio_file_to_features(audio_path, config)
-    logger.info("audio file loaded and feature computation success : %s" % audio_path)
 
-    # Majmin type chord recognition
-    feature = feature.T
-    feature = (feature - mean) / std
-    time_unit = feature_per_second
-    n_timestep = config.model['timestep']
-
-    num_pad = n_timestep - (feature.shape[0] % n_timestep)
-    feature = np.pad(feature, ((0, num_pad), (0, 0)), mode="constant", constant_values=0)
-    num_instance = feature.shape[0] // n_timestep
-
-    start_time = 0.0
-    lines = []
-    with torch.no_grad():
-        model.eval()
-        feature = torch.tensor(feature, dtype=torch.float32).unsqueeze(0).to(device)
-        for t in range(num_instance):
-            self_attn_output, _ = model.self_attn_layers(feature[:, n_timestep * t:n_timestep * (t + 1), :])
-            prediction, _ = model.output_layer(self_attn_output)
-            prediction = prediction.squeeze()
-            for i in range(n_timestep):
-                if t == 0 and i == 0:
-                    prev_chord = prediction[i].item()
-                    continue
-                if prediction[i].item() != prev_chord:
-                    lines.append(
-                        '%.3f %.3f %s\n' % (start_time, time_unit * (n_timestep * t + i), idx_to_chord[prev_chord]))
-                    start_time = time_unit * (n_timestep * t + i)
-                    prev_chord = prediction[i].item()
-                if t == num_instance - 1 and i + num_pad == n_timestep:
-                    if start_time != time_unit * (n_timestep * t + i):
-                        lines.append('%.3f %.3f %s\n' % (start_time, time_unit * (n_timestep * t + i), idx_to_chord[prev_chord]))
-                    break
-
-    # lab file write
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
     save_path = os.path.join(args.save_dir, os.path.split(audio_path)[-1].replace('.mp3', '').replace('.wav', '') + '.lab')
-    with open(save_path, 'w') as f:
-        for line in lines:
-            f.write(line)
 
-    logger.info("label file saved : %s" % save_path)
+    # if already processed
+    if os.path.exists(save_path):
+        logger.info("label file %s already saved !" % save_path)
+        continue
+    else:
 
-    # lab file to midi file
+        feature, feature_per_second, song_length_second = audio_file_to_features(audio_path, config)
+        logger.info("audio file loaded and feature computation success : %s" % audio_path)
+
+        # Majmin type chord recognition
+        feature = feature.T
+        feature = (feature - mean) / std
+        time_unit = feature_per_second
+        n_timestep = config.model['timestep']
+
+        num_pad = n_timestep - (feature.shape[0] % n_timestep)
+        feature = np.pad(feature, ((0, num_pad), (0, 0)), mode="constant", constant_values=0)
+        num_instance = feature.shape[0] // n_timestep
+
+        start_time = 0.0
+        lines = []
+        with torch.no_grad():
+            model.eval()
+            feature = torch.tensor(feature, dtype=torch.float32).unsqueeze(0).to(device)
+            for t in range(num_instance):
+                self_attn_output, _ = model.self_attn_layers(feature[:, n_timestep * t:n_timestep * (t + 1), :])
+                prediction, _ = model.output_layer(self_attn_output)
+                prediction = prediction.squeeze()
+                for i in range(n_timestep):
+                    if t == 0 and i == 0:
+                        prev_chord = prediction[i].item()
+                        continue
+                    if prediction[i].item() != prev_chord:
+                        lines.append(
+                            '%.3f %.3f %s\n' % (start_time, time_unit * (n_timestep * t + i), idx_to_chord[prev_chord]))
+                        start_time = time_unit * (n_timestep * t + i)
+                        prev_chord = prediction[i].item()
+                    if t == num_instance - 1 and i + num_pad == n_timestep:
+                        if start_time != time_unit * (n_timestep * t + i):
+                            lines.append('%.3f %.3f %s\n' % (start_time, time_unit * (n_timestep * t + i), idx_to_chord[prev_chord]))
+                        break
+
+        # lab file write
+        
+        with open(save_path, 'w') as f:
+            for line in lines:
+                f.write(line)
+
+        logger.info("label file saved : %s" % save_path)
     
+        # lab file to midi file
+        
 
-    starts, ends, pitchs = list(), list(), list()
+        # starts, ends, pitchs = list(), list(), list()
 
-    intervals, chords = mir_eval.io.load_labeled_intervals(save_path)
-    for p in range(12):
-        for i, (interval, chord) in enumerate(zip(intervals, chords)):
-            root_num, relative_bitmap, _ = mir_eval.chord.encode(chord)
-            tmp_label = mir_eval.chord.rotate_bitmap_to_root(relative_bitmap, root_num)[p]
-            if i == 0:
-                start_time = interval[0]
-                label = tmp_label
-                continue
-            if tmp_label != label:
-                if label == 1.0:
-                    starts.append(start_time), ends.append(interval[0]), pitchs.append(p + 48)
-                start_time = interval[0]
-                label = tmp_label
-            if i == (len(intervals) - 1): 
-                if label == 1.0:
-                    starts.append(start_time), ends.append(interval[1]), pitchs.append(p + 48)
+        # intervals, chords = mir_eval.io.load_labeled_intervals(save_path)
+        # for p in range(12):
+        #     for i, (interval, chord) in enumerate(zip(intervals, chords)):
+        #         root_num, relative_bitmap, _ = mir_eval.chord.encode(chord)
+        #         tmp_label = mir_eval.chord.rotate_bitmap_to_root(relative_bitmap, root_num)[p]
+        #         if i == 0:
+        #             start_time = interval[0]
+        #             label = tmp_label
+        #             continue
+        #         if tmp_label != label:
+        #             if label == 1.0:
+        #                 starts.append(start_time), ends.append(interval[0]), pitchs.append(p + 48)
+        #             start_time = interval[0]
+        #             label = tmp_label
+        #         if i == (len(intervals) - 1): 
+        #             if label == 1.0:
+        #                 starts.append(start_time), ends.append(interval[1]), pitchs.append(p + 48)
 
-    midi = pm.PrettyMIDI()
-    instrument = pm.Instrument(program=0)
+        # midi = pm.PrettyMIDI()
+        # instrument = pm.Instrument(program=0)
 
-    for start, end, pitch in zip(starts, ends, pitchs):
-        pm_note = pm.Note(velocity=120, pitch=pitch, start=start, end=end)
-        instrument.notes.append(pm_note)
+        # for start, end, pitch in zip(starts, ends, pitchs):
+        #     pm_note = pm.Note(velocity=120, pitch=pitch, start=start, end=end)
+        #     instrument.notes.append(pm_note)
 
-    midi.instruments.append(instrument)
-    midi.write(save_path.replace('.lab', '.midi'))    
+        # midi.instruments.append(instrument)
+        # midi.write(save_path.replace('.lab', '.midi'))    
 
